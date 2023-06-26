cbff83507ff5a085217eaeb7234206a9cf359dc3
dcatcherex <dcatcherex@yahoo.com>
2023-06-26 16:05:45 +0700

    update

--- src/components/TemplateSet/TemplateSet.jsx
+++ src/components/TemplateSet/TemplateSet.jsx
@@ -1,6 +1,12 @@
 import viewicon from "../../assets/images/view.svg";
 import link from "../../assets/images/link.svg";
 import exam from "../../assets/images/exam.svg";
+
+import allicon from "../../assets/images/icons/all.svg";
+import educationicon from "../../assets/images/icons/education.svg";
+import socialicon from "../../assets/images/icons/social.svg";
+import writingicon from "../../assets/images/icons/writing.svg";
+
 import quiz from "../../assets/images/quiz.png";
 import ExamType from "../ExamType/ExamType";
 import { useState } from "react";
@@ -29,18 +35,18 @@ const TemplateSet = ({jsonData}) => {
   return (
     <div className="min-h-screen">
       {/* button filter */}
-      <div className="flex justify-center pt-4">
-        <button className="btn px-4 py-2 min-w-[70px] rounded-full" onClick={()=> setFilterValue("all")}>All</button>
-        <button className="btn px-4 py-2 min-w-[70px] rounded-full" onClick={()=> setFilterValue("writing")}>Writing</button>
-        <button className="btn px-4 py-2 min-w-[70px] rounded-full" onClick={()=> setFilterValue("social")}>Social</button>
-        <button className="btn px-4 py-2 min-w-[70px] rounded-full" onClick={()=> setFilterValue("education")}>Education</button>
+      <div className=" flex justify-center pt-4">
+        <button className=" px-4 py-2 min-w-[70px] hover:scale-125 transition-transform text-darkviolet" onClick={()=> setFilterValue("all")}><img src={allicon} className="w-20 h-20" alt="all icon"/> All</button>
+        <button className=" px-4 py-2 min-w-[70px] hover:scale-125 transition-transform text-darkviolet" onClick={()=> setFilterValue("writing")}><img src={writingicon} className="w-20 h-20" alt="all icon"/>Writing</button>
+        <button className=" px-4 py-2 min-w-[70px] hover:scale-125 transition-transform text-darkviolet" onClick={()=> setFilterValue("social")}><img src={socialicon} className="w-20 h-20" alt="all icon"/>Social</button>
+        <button className=" px-4 py-2 min-w-[70px] hover:scale-125 transition-transform text-darkviolet" onClick={()=> setFilterValue("education")}><img src={educationicon} className="w-20 h-20" alt="all icon"/>Education</button>
       </div>
       
       <main className=" max-w-[1280px] mx-auto bg-violet-100 grid grid-cols-2 grid- sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 pt-2 mt-2 px-4 ">
         {filteredData.map(({ id, title, des, view }) => (
           <motion.div
             key={id}
-            className="gap-x-2 px-2 py-1  bg-white rounded-md shadow-md hover:ring-2 hover:cursor-pointer hover:ring-violet-500 active:bg-violet-100" initial="hidden"
+            className="gap-x-2 px-2 py-1  bg-white rounded-md shadow-md hover:ring-2 hover:cursor-pointer hover:ring-violet-500 active:bg-violet-100 " initial="hidden"
             animate="visible" variants={variants}
           >
               <div className="">
