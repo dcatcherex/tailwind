beb90be5c22caa73fb940b7c08590909aa4d11bc
dcatcherex <dcatcherex@yahoo.com>
2023-06-30 00:49:27 +0700

    update

--- src/components/Exam/TakeExam.jsx
+++ src/components/Exam/TakeExam.jsx
@@ -7,6 +7,15 @@ import TimeBar from "./TimeBar";
 import ExamHead from "./ExamHead"
 import data from "../../assets/datas/samplequestions.json";
 
+import btnfour from "../../assets/images/gameinterface/btnfour.png"
+import btnheart from "../../assets/images/gameinterface/btnheart.png"
+import btnone from "../../assets/images/gameinterface/btnone.png"
+import btnrandom from "../../assets/images/gameinterface/btnrandom.png"
+import btnthree from "../../assets/images/gameinterface/btnthree.png"
+import btntwo from "../../assets/images/gameinterface/btntwo.png"
+import arrowright from "../../assets/images/gameinterface/arrowright.png"
+import arrowleft from "../../assets/images/gameinterface/arrowleft.png"
+
 const TakeExam = () => {
    const [questions, setQuestions] = useState(data);
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
@@ -30,28 +39,28 @@ const TakeExam = () => {
   }, []);
 
   // Socket connection
-  useEffect(() => {
-    const s = io("http://localhost:3000"); // Connect to server (use your server URL)
-    setSocket(s);
+  // useEffect(() => {
+  //   const s = io("http://localhost:3000"); // Connect to server (use your server URL)
+  //   setSocket(s);
 
-    axios
-      .get("http://127.0.0.1:3000/quiz")
-      .then((response) => {
-        setQuestions(response.data);
-      })
-      .catch((error) => console.error(`Error fetching data: ${error}`));
+  //   axios
+  //     .get("http://127.0.0.1:3000/quiz")
+  //     .then((response) => {
+  //       setQuestions(response.data);
+  //     })
+  //     .catch((error) => console.error(`Error fetching data: ${error}`));
 
-    s.on("feedback", (data) => {
-      setFeedback((prevFeedback) => ({
-        ...prevFeedback,
-        [data.questionId]: data.isCorrect ? "Correct!" : "Incorrect",
-      }));
-    });
+  //   s.on("feedback", (data) => {
+  //     setFeedback((prevFeedback) => ({
+  //       ...prevFeedback,
+  //       [data.questionId]: data.isCorrect ? "Correct!" : "Incorrect",
+  //     }));
+  //   });
 
-    return () => {
-      s.disconnect();
-    };
-  }, []);
+  //   return () => {
+  //     s.disconnect();
+  //   };
+  // }, []);
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -86,7 +95,7 @@ const TakeExam = () => {
         {questions.length > 0 && (
           <>
             <form
-              className="max-w-[500px]  mx-auto mt-10 bg-white font-body rounded-lg text-lg mb-4"
+              className=" max-w-[500px]  mx-auto mt-10 bg-white font-body rounded-lg text-lg mb-4"
               onSubmit={handleSubmit}
             >
               <div className="flex px-2 py-3 min-h-[80px] bg-blue-500 text-white tracking-wide rounded-t-lg">
@@ -123,14 +132,30 @@ const TakeExam = () => {
             {/* A list of questions */}
           </>
         )}
-        <button className="invisible sm:visible sm:fixed sm:bottom-10 right-10 bg-blue-500 text-white rounded-full p-4">
-          dsdfs
-        </button>
-        <PaginatedList
-          jsonData={questions}
-          questionSelect={handleQuestionSelect} // Pass the handleQuestionSelect function as the questionSelect prop
-          selectedOptions={selectedOptions} // Pass the selectedOptions object as a prop
-        />
+        <div className="">
+          <button className="invisible sm:visible sm:fixed sm:bottom-[20%] left-[28%] rotate-45 rounded-full text-white   hover:ring-2 hover:ring-dimviolet"><img src={arrowleft} className="w-20" alt="" />
+          </button>
+          <button className="invisible sm:visible sm:fixed sm:bottom-[13%] left-[32%] rotate-45 rounded-full text-white   hover:ring-2 hover:ring-dimviolet"><img src={arrowright} className="w-20" alt="" />
+          </button>
+          <button className="invisible sm:visible sm:fixed sm:bottom-[23%] left-[70%] rounded-full text-white   hover:ring-2 hover:ring-dimviolet"><img src={btnone} className="w-12" alt="" />
+          </button>
+          <button className="invisible sm:visible sm:fixed sm:bottom-[20%] left-[67%] rounded-full text-white   hover:ring-2 hover:ring-dimviolet"><img src={btntwo} className="w-12" alt="" />
+          </button>
+          <button className="invisible sm:visible sm:fixed sm:bottom-[15.5%] left-[65%] rounded-full text-white   hover:ring-2 hover:ring-dimviolet"><img src={btnthree} className="w-12" alt="" />
+          </button>
+          <button className="invisible sm:visible sm:fixed sm:bottom-[10%] left-[64%] rounded-full text-white   hover:ring-2 hover:ring-dimviolet"><img src={btnfour} className="w-12" alt="" />
+          </button>
+          <button className="invisible sm:visible sm:fixed sm:bottom-[10%] left-[68%] rounded-full text-white p-1  hover:ring-2 hover:ring-dimviolet"><img src={btnrandom} className="w-24" alt="" />
+          </button>
+          
+        </div>
+        <div className="">
+          <PaginatedList
+            jsonData={questions}
+            questionSelect={handleQuestionSelect} // Pass the handleQuestionSelect function as the questionSelect prop
+            selectedOptions={selectedOptions} // Pass the selectedOptions object as a prop
+          />
+        </div>
       </div>
     </div>
   );
